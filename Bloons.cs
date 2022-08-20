f4bb37b0e4aae6c25006c17c7832b4b3b0b12bfd
Mani_Dev <dhingramsp@Gmail.com>
2022-08-20 21:52:34 +0530

    added 2 bloons
    Added the legendary camgrow fortified red, and an orange bloon with a projectile shield

--- Bloons.cs
+++ Bloons.cs
@@ -1,8 +1,13 @@
 ﻿using Assets.Scripts.Models.Bloons;
+using Assets.Scripts.Models.Bloons.Behaviors;
+using Assets.Scripts.Models.Bloons.Behaviors.Actions;
 using Assets.Scripts.Unity.Display;
 using BTD_Mod_Helper.Api.Bloons;
 using BTD_Mod_Helper.Api.Display;
 using BTD_Mod_Helper.Api.Enums;
+using BTD_Mod_Helper.Extensions;
+using Harmony;
+using UnhollowerBaseLib;
 
 namespace CustomBloons
 {
@@ -28,11 +33,14 @@ namespace CustomBloons
     }
     class OrangeBloon : ModBloon
     {
-        public override string BaseBloon => BloonType.Black;
+        public override string BaseBloon => BloonType.Rainbow;
         public override string Icon => "Orange";
         public override bool UseIconAsDisplay => true;
         public override void ModifyBaseBloonModel(BloonModel bloon)
         {
+            bloon.maxHealth = 10;
+            bloon.AddBehavior(new ReflectProjectilesInRadiusActionModel("OrangeBloonReflectProjectilesInRadiusActionModel", "OrangeBloonShield", 99999999, 15, 15, 0.6f));
+            bloon.AddBehavior(new TimeTriggerModel("OrangeBloonHealthTimeTriggerModel", 99999999, true, new string[] { "OrangeBloonShield" }));
         }
     }
     class OrangeBloonDisplay : ModBloonDisplay<OrangeBloon>
